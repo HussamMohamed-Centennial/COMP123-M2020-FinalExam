253ee166e76149124bcc73e812c49c382fe59e9d
Hussam Eldin Mohamed <dr.hussam@live.com>
2020-08-19 23:25:48 -0400

    Add project files.

---
+++ COMP123-S2016-FinalExam/SplashForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_FinalExam
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// splash timer tick event handler will show next form ,hide this form ,also will disable timer to prevent repeated action when the timer interval end.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SplashFormTimer_Tick(object sender, EventArgs e)
+        {
+            Program.generateNameForm.Show();
+            this.Hide();
+            SplashFormTimer.Enabled = false;
+        }
+    }
+}
